cde249b7cb212047adf4206a409da30d6fc33d8a
Engin ÇAKIR <engincakir830@gmail.com>
2023-08-13 20:44:14 +0300

    Update

--- src/map_example/Main.java
+++ src/map_example/Main.java
@@ -8,16 +8,17 @@ public class Main {
     public static void main(String[] args) {
 
         HashMap<String, Integer> words = new HashMap<>();
-        String RandomWord = "Put an and to";
-        for (int i = 0; i < RandomWord.length(); i++) {
+        String randomWord = "Put an and to";
+        for (int i = 0; i < randomWord.length(); i++) {
             int count = 1;
-            for (int j = i + 1; j < RandomWord.length(); j++) {
-                if (RandomWord.charAt(i) == RandomWord.charAt(j)) {
+            for (int j = i + 1; j < randomWord.length(); j++) {
+                if (randomWord.charAt(i) == randomWord.charAt(j)) {
                     count++;
                 }
 
             }
-            words.putIfAbsent(String.valueOf(RandomWord.charAt(i)), count);
+            if (randomWord.charAt(i) != ' ')
+                words.putIfAbsent(String.valueOf(randomWord.charAt(i)), count);
 
         }
         System.out.println(words.entrySet());
